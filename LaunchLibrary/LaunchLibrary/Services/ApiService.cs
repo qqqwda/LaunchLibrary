081a941be406b0d07be0f697ea130c3b4adbbb6a
Daniel Vargas <41929135+qqqwda@users.noreply.github.com>
2018-11-08 14:56:44 -0400

    ahora si

--- LaunchLibrary/LaunchLibrary/Services/ApiService.cs
+++ LaunchLibrary/LaunchLibrary/Services/ApiService.cs
@@ -50,29 +50,44 @@ namespace LaunchLibrary.Services
 
         public async Task<Response> GetList<T>(string urlBase, string servicePrefix, string controller)
         {
-            var client = new HttpClient();
-            client.BaseAddress = new Uri(urlBase);
-            var url = string.Format("{0}{1}",servicePrefix, controller);
-            var response = await client.GetAsync(url);
-            var result = await response.Content.ReadAsStringAsync();
+            try
+            {
+                var client = new HttpClient();
+                client.BaseAddress = new Uri(urlBase);
+                var url = string.Format("{0}{1}", servicePrefix, controller);
+                var response = await client.GetAsync(url);
+                var result = await response.Content.ReadAsStringAsync();
 
-            if (!response.IsSuccessStatusCode)
+                if (!response.IsSuccessStatusCode)
+                {
+                    return new Response
+                    {
+                        IsSuccess = false,
+                        Message = result
+                    };
+                }
+
+                var list = JsonConvert.DeserializeObject<List<T>>(result);
+
+                return new Response
+                {
+                    IsSuccess = true,
+                    Message = "OK",
+                    Result = list
+                };
+
+            }
+            catch (Exception ex)
             {
                 return new Response
                 {
                     IsSuccess = false,
-                    Message = result
+                    Message = ex.Message,
                 };
             }
 
-            var list = JsonConvert.DeserializeObject<List<T>>(result);
 
-            return new Response
-            {
-                IsSuccess = true,
-                Message = "OK",
-                Result = list
-            };
+            
         }
 
         #endregion
